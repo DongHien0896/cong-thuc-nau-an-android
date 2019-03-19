f13697c8bb094eb21a7cb0236000d96bed8d8e0d
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-03-19 17:02:17 +0700

    Fix main screen

--- app/src/main/java/com/amthuc/congthuc/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/amthuc/congthuc/ui/screen/main/MainActivity.kt
@@ -3,8 +3,10 @@ package com.amthuc.congthuc.ui.screen.main
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.os.bundleOf
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,12 +16,15 @@ import com.amthuc.congthuc.R
 import com.amthuc.congthuc.data.model.Category
 import com.amthuc.congthuc.databinding.ActivityMainBinding
 import com.amthuc.congthuc.ui.base.BaseActivity
+import com.amthuc.congthuc.ui.screen.recipe.RecipeFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override val viewModel: MainViewModel by viewModel()
 
@@ -29,7 +34,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         super.onCreate(savedInstanceState)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val adapter = CategoryDrawerAdapter(::openCategoryDetail)
 
         setSupportActionBar(toolbar)
@@ -78,6 +83,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     }
 
     private fun openCategoryDetail(category: Category) {
-
+        findNavController(R.id.nav_host_fragment).navigate(
+            R.id.recipe_dest,
+            bundleOf(
+                RecipeFragment.ARGUMENT_CATEGORY to category,
+                RecipeFragment.ARGUMENT_TITLE to category.title
+            )
+        )
+        drawerLayout.closeDrawers()
     }
 }
