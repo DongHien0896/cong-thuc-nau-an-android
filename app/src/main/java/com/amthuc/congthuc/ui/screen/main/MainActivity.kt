63e66807144608a6bc53768f6c3e5554a71f94d5
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-03-21 09:08:06 +0700

    Fix drawer navigate

--- app/src/main/java/com/amthuc/congthuc/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/amthuc/congthuc/ui/screen/main/MainActivity.kt
@@ -2,15 +2,16 @@ package com.amthuc.congthuc.ui.screen.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.os.bundleOf
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,7 +26,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
 
     override val viewModel: MainViewModel by viewModel()
@@ -38,9 +39,12 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         drawerLayout = findViewById(R.id.drawer_layout)
         val adapter = CategoryDrawerAdapter(::openCategoryDetail)
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+                ?: return
+        navController = host.navController
 
         setSupportActionBar(toolbar)
-        setupDrawer(toolbar, drawerLayout)
         setupActionBar()
         setupRecyclerCategoryDrawer(adapter)
 
@@ -68,25 +72,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
         }
     }
 
-    private fun setupActionBar() {
-        val host: NavHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
-                ?: return
-        val navController = host.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    private fun setupDrawer(toolbar: Toolbar, drawerLayout: DrawerLayout) {
-        val toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            toolbar,
-            R.string.nav_app_bar_open_drawer_description,
-            R.string.nav_app_bar_navigate_up_description
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+    private fun setupActionBar() {
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     private fun setupRecyclerCategoryDrawer(adapter: CategoryDrawerAdapter) {
